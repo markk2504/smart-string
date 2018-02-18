e7300a0c5f051fa883267ed7c04d028e46ff48da
Mark Kaplan <markkaplan@dropbox.com>
2018-02-18 10:51:54 +0200

    Imitial version of the setup file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='smart-string',
+      version='0.1',
+      description='Smart String implementation',
+      url='https://github.com/markk2504/smart-string',
+      author='Mark Kaplan',
+      author_email='markk2504@gmail.com',
+      packages=['smart_str'],
+      zip_safe=False)
