0526421c2e6fbe4572381184ad283baa09a6a30f
Mark Kaplan <markk2504@gmail.com>
2018-08-29 17:50:08 +0300

    Version upgrade.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(current_dir, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='smart-string',
-      version='0.0.3',
+      version='0.0.4',
       description='Smart String implementation',
       long_description=long_description,
       long_description_content_type='text/markdown',
