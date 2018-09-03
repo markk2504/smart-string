b5d74070c014808c7dfb1eaa15fc8bf5308e0206
Mark Kaplan <markk2504@gmail.com>
2018-09-03 17:16:41 +0300

    documentation updated.

--- dev_scripts/Windows/test_upload_all_releases.bat
+++ dev_scripts/Windows/test_upload_all_releases.bat
@@ -3,9 +3,9 @@ title Upload all Releases to Test PYPI
 twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 
 rem To install smart-string from the test PYPI:
-rem pip install --index-url https://test.pypi.org/simple/ smart-string
+rem pip install -U --index-url https://test.pypi.org/simple/ smart-string
 
 rem To install smart-string from the test PYPI, and the dependencies from the production PYPI:
-rem pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple smart-string
+rem pip install -U --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple smart-string
 
 pause
--- dev_scripts/Windows/test_upload_release.bat
+++ dev_scripts/Windows/test_upload_release.bat
@@ -2,4 +2,10 @@ title Upload Release to Test PYPI
 
 twine upload --repository-url https://test.pypi.org/legacy/ %1
 
+rem To install smart-string from the test PYPI:
+rem pip install -U --index-url https://test.pypi.org/simple/ smart-string
+
+rem To install smart-string from the test PYPI, and the dependencies from the production PYPI:
+rem pip install -U --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple smart-string
+
 pause
